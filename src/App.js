627359ed5962927e68ecb6d5265bdbb6474ffc6e
RiyaSrivastava12 <srivastava.riya12@gmail.com>
2021-01-13 17:45:10 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: null,
+      password: null,
+      formErrors: {
+        username: "",
+        password: ""
+      }
+    };
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="form-wrapper">
+        <div className="pic">
+        <h1><b>SIGN IN</b></h1>
+      </div>
+          
+          <form onSubmit={this.handleSubmit} noValidate>
+            <div className="username">
+              <label htmlFor="username">UserName</label>
+              <input
+                placeholder="Enter Username"
+                type="text"
+                name="username"  
+              /> 
+            </div> 
+            <div className="password">
+              <label htmlFor="password">Password</label>
+              <input
+                placeholder="Enter Password"
+                type="password"
+                name="password"
+                noValidate 
+              />
+              
+            </div>
+            <div className="remember">
+            
+            <input type="checkbox" name="rem"/>
+            <label htmlFor="remember">Remember Me</label>
+            <a href="#">Forgot Password?</a>
+            </div>
+            <div className="login">
+            
+              <button type="submit">Login</button>
+              
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
